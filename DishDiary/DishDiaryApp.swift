0866e7036d234cd516345db584f21e6a4e655842
Daria Yats <dariayatsyniuk@gmail.com>
2025-08-11 16:12:23 +0200

    Initial Commit

---
+++ DishDiary/DishDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishDiaryApp.swift
+//  DishDiary
+//
+//  Created by Daria Yatsyniuk on 11.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DishDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
